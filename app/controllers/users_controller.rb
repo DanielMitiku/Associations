f1ff44935f79e1a952d761c7c82a96a3528f5faa
DanielMitiku <wedanielmitiku@gmail.com>
2019-08-28 15:57:50 +0300

    added login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
 @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-flash[:success] = "Welcome to the Private Events website!"
+    flash[:success] = "Welcome to the Private Events website!"
+    log_in(@user)
     redirect_to @user
 
       # Handle a successful save.
--- db/schema.rb
+++ db/schema.rb
@@ -29,13 +29,6 @@ ActiveRecord::Schema.define(version: 2019_08_27_104813) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table "invitations", force: :cascade do |t|
-    t.integer "attendee_id"
-    t.integer "attended_event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
