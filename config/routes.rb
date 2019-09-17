b61cf24ec503c5e31ec8894e7c930adeb55b2020
ibtechraj <raj_shk@rediffmail.com>
2019-09-17 15:00:06 +0530

    indenting done

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,8 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
-#  get 'users/new'
-#  get 'users/create'
-#  get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'
   resources :users
-  
   resources :events
   root 'events#index'
 end
